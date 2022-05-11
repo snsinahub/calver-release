0210ae0304ba798247d037ba85cd7657016964c6
Siavash Namvar <snsinahub@github.com>
2022-05-11 17:40:29 -0400

    add all files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # calver-with-release
+
+```YAML
+steps:
+  - name: checkout
+      uses: actions/checkout@v2
+    - name: move tag
+      uses: snsinahub-org/calver-with-release@main
+      with:
+        tag: "tag_name"
+        repo: "${{ github.repository }}"
+        token: "${{ github.GITHUB_TOKEN }}"
+```
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const {_} = require('lodash')
+const github = require('@actions/github');
+const core = require('@actions/core');
+const { exec } = require("child_process");
+var args = process.argv.slice(2);
+
+
+
+async function run() {
+    const myToken = core.getInput('token');
+    const repoFull = core.getInput('repo').split('/');
+    const tag = core.getInput('tag');
+    const allowedUsers = core.getInput('allowed_users');
+    const repo = repoFull[1]
+    const owner = repoFull[2]
+
+
+    
+    const octokit = github.getOctokit(myToken)
+
+    const ooutput = octokit.rest.repos.getReleaseByTag({
+        owner,
+        repo,
+        tag,
+      });
+}
+
+run();
