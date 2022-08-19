831643dc7cf095663c4b5931b6c02a36f076e6f7
Siavash Namvar <snsinahub@github.com>
2022-08-19 14:53:48 -0400

    add console.log

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ async function run() {
         })
     
     let matches = _.filter(output['data'], function(obj){
+        console.log("OBJ: " + JSON.stringify(obj))
+        console.log("OBJ NAME: " + obj.name)
         return obj.name.includes(today)
     })
     console.log("output: " + JSON.stringify(output['data']))
